1f09c17a1a954f4e812b2ae540db55ff413a0b75
jorge <jresino@gmail.com>
2024-06-13 23:38:58 +0200

    readme

--- api.py
+++ api.py
@@ -1,22 +1,7 @@
 import os
-import json
 import logging
-from termcolor import colored
 from dotenv import load_dotenv
-from langchain_community.embeddings import GPT4AllEmbeddings
-from langchain.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI
-from dataclasses import dataclass
-from typing import Dict, List, Tuple, Union, Optional, Callable, ClassVar
-from langchain.chains.llm import LLMChain
-from pydantic import BaseModel, ValidationError
-from src.model.chains import get_analyzer_chain
-from src.model.graph import create_graph, compile_workflow
-from src.model.states import (
-    Analisis,
-    Candidato,
-    State
-)
+
 from src.model.utils import (
                         get_current_spanish_date_iso, 
                         setup_logging,
@@ -24,7 +9,6 @@ from src.model.utils import (
                         get_arg_parser
                         )
 from src.backend.fast_api import run_fast_api
-from src.model.exceptions import NoOpenAIToken, JsonlFormatError
 
 # Load environment variables from .env file
 load_dotenv()
@@ -41,7 +25,6 @@ logger = logging.getLogger(__name__)
 def main():
     setup_logging()
     run_fast_api()
-    
     
 if __name__ == '__main__':
     main()
