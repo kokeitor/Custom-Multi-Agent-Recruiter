ad0e00bfaf18160bcfb76134927b3f1b7323a776
jorge <jresino@gmail.com>
2024-06-29 18:16:31 +0200

    b

--- src/app/chatbot.py
+++ src/app/chatbot.py
@@ -219,8 +219,9 @@ def run_app(compiled_graph : CompiledGraph, config : modes.ConfigGraphApi, graph
                                 )
                     if graph_image:
                         with st.container():
-                            st.markdown("**Grafo Multi-Agente utilizado para el análisis:**")
-                            st.image(image=graph_image)
+                            display_graph = st.button("**Grafo Multi-Agente utilizado para el análisis:**")
+                            if display_graph:
+                                st.image(image=graph_image)
                             
             else:
                 st.error("Debes aceptar la politica de la empresa para continuar con el análisis")
