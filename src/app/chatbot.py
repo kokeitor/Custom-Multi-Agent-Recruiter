da7efe8d7cbb75ba51c71fe065395ddc9f1fa6b3
jorge <jresino@gmail.com>
2024-06-30 14:14:08 +0200

    logger

--- src/app/chatbot.py
+++ src/app/chatbot.py
@@ -215,6 +215,9 @@ def run_app(config_graph_path : str) -> None:
             model_available = True
             
         elif option is not None and str(option) == MODELS[2]:
+            st.error(f"{option} no disponible ¡Comming soon!")
+            
+            """
             st.success(f"{option} disponible")
             
             # Modify AgentConfigApi object atributes : Change analyzer agent atribute instance or object 
@@ -229,7 +232,8 @@ def run_app(config_graph_path : str) -> None:
             graph_png = get_png_graph(compiled_graph)
             logger.info("Graph and workflow created")
             model_available = True
-            
+            """
+
         elif option is not None and str(option) == MODELS[3]:
             st.error(f"{option} no disponible ¡Comming soon!")
 
